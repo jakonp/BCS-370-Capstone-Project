1c3e1aa37971fd4d413ce6c0f15add9de5592436
Nick Jakobczyk <jakonp@protonmail.ch>
2020-11-17 19:24:17 -0500

    added Assignment Class (untested)

---
+++ BCS 370 Capstone Project/BCS 370 Capstone Project/class.cpp
@@ -0,0 +1,80 @@
+//
+//  class.cpp
+//  BCS 370 Capstone Project
+//
+//  Created by Nick Jakobczyk on 11/17/20.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Assignment
+{
+public:
+    string course;
+    string description;
+    int month;
+    int day;
+private:
+    void addAssignment()
+    {
+        cout << "\nEnter Course: ";
+        cin >> this->course;
+        cout << "\nEnter Description: ";
+        cin >> this->description;
+        cout << "\nEnter month due: ";
+        cin >> this->month;
+        cout << "\nEnter day due: ";
+        cin >> this->day;
+    }
+    void updateAssignment()
+    {
+        int choice;
+        cout << "What would you like to change?";
+        cout << "\n1. Course\n2. Description\n3. Month Due\n4.Day Due\n5. Exit";
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+            {
+                cout << "What is the new course?: ";
+                string newCourse;
+                cin >> newCourse;
+                this->course = newCourse;
+                break;
+            }
+            case 2:
+            {
+                cout << "What is the new description?: ";
+                string newDescription;
+                cin >> newDescription;
+                this->description = newDescription;
+                break;
+            }
+            case 3:
+            {
+                cout << "What is the new month due?: ";
+                int newMonth;
+                cin >> newMonth;
+                this->month = newMonth;
+                break;
+            }
+            case 4:
+            {
+                cout << "What is the new day due?: ";
+                int newDay;
+                cin >> newDay;
+                this->day = newDay;
+                break;
+            }
+            case 5:
+            {
+                break;
+            }
+            
+        }
+    }
+    
+};
--- BCS 370 Capstone Project/BCS 370 Capstone Project/main.cpp
+++ BCS 370 Capstone Project/BCS 370 Capstone Project/main.cpp
@@ -9,10 +9,11 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <stack>
 
 using namespace std;
 
 int main() {
     //insert code here
-    
+   
 }
