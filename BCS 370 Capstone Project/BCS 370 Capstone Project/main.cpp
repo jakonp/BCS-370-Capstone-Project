1b6a8cdc7baa300d52ab87c430fbe406b604c3e7
Nick Jakobczyk <jakonp@protonmail.ch>
2020-11-18 13:36:35 -0500

    Started working on the User Interface

--- BCS 370 Capstone Project/BCS 370 Capstone Project/main.cpp
+++ BCS 370 Capstone Project/BCS 370 Capstone Project/main.cpp
@@ -20,28 +20,77 @@ void displayVector(vector<Assignment> v)
 {
     for(int i = 0; i < v.size(); i++)
     {
+        cout << "Assignment #" << i+1 << endl;
         Assignment temp = v[i];
         temp.displayAssignment();
         cout << endl;
     }
 }
 
+vector<Assignment> myVector;
+
+void mainMenu()
+{
+    int choice;
+    cout << "-=-=-HOMEWORK MANAGER-=-=-" << endl;
+    cout << "1. Add Assignmet" << endl;
+    cout << "2. Edit Assignment" << endl;
+    cout << "3. View All Assignments" << endl;
+    cout << "4. Remove Assignment" << endl;
+    cout << "5. Search Assignment" << endl;
+    cout << "6. QUIT" << endl;
+    cin >> choice;
+    
+    switch(choice)
+    {
+        case 1:
+        {
+            Assignment temp;
+            temp.addAssignment();
+            myVector.push_back(temp);
+            mainMenu();
+            break;
+        }
+        case 2:
+        {
+            displayVector(myVector);
+            cout << "Select Assignment to edit: ";
+            int assignmentNum;
+            cin >> assignmentNum;
+            Assignment temp = myVector[assignmentNum-1];
+            temp.updateAssignment();
+            myVector[assignmentNum-1] = temp;
+            mainMenu();
+            break;
+        }
+        case 3:
+        {
+            displayVector(myVector);
+            mainMenu();
+            break;
+        }
+        case 4:
+        {
+            displayVector(myVector);
+            cout << "Select Assignment to remove: ";
+            int assignmentNum;
+            cin >> assignmentNum;
+            myVector.erase(myVector.begin()-1);
+            mainMenu();
+            break;
+        }
+    }
+    
+    
+}
+
+
 
 int main() {
     
     //Currently Test Code
-    vector<Assignment> myVector;
-    
-    Assignment Ass1("math", "Study", 3, 24, 2, false);
-    myVector.push_back(Ass1);
-    
-    Assignment Ass2("english", "read", 4, 2, 14, true);
-    myVector.push_back(Ass2);
-    
-    Assignment Ass3("coding", "code", 2, 4, 12, false);
-    myVector.push_back(Ass3);
-    
-    displayVector(myVector);
+    mainMenu();
+ 
     
     
 }
